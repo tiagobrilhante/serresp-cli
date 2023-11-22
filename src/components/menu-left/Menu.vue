0efb04cc2bf45123c8338102a506012c978bfb95
Tiago Brilhante <tiagobrilhantemania@gmail.com>
2023-11-22 15:46:12 -0400

    update

--- src/components/menu-left/Menu.vue
+++ src/components/menu-left/Menu.vue
@@ -5,51 +5,41 @@
              :src="require('@/assets/img/logoUninorte.jpg')"
       />
       <h2>Menu</h2>
-      <v-divider class="mb-4"></v-divider>
-      <v-btn to="/" color="secondary" block max-width="100%"> Página Incial</v-btn>
-      <v-btn class="mt-3" color="secondary" block max-width="100%"> O que é SerResponsável</v-btn>
-      <v-btn to="/destaques" class="mt-3" color="secondary" block max-width="100%"> Em destaque </v-btn>
-      <v-btn to="/eventos" class="mt-3" color="secondary" block max-width="100%"> Ao longo dos anos</v-btn>
-      <v-btn class="mt-3" color="secondary" block max-width="100%"> Benefícios do Projeto</v-btn>
+      <v-divider v-if="!usuarioEstaLogado" class="mb-4"></v-divider>
+      <v-btn v-if="!usuarioEstaLogado" to="/" color="secondary" block max-width="100%"> Página Incial</v-btn>
+      <v-btn v-if="!usuarioEstaLogado" class="mt-3" color="secondary" block max-width="100%"> O que é SerResponsável</v-btn>
+      <v-btn v-if="!usuarioEstaLogado" to="/destaques" class="mt-3" color="secondary" block max-width="100%"> Em destaque</v-btn>
+      <v-btn v-if="!usuarioEstaLogado" to="/eventos" class="mt-3" color="secondary" block max-width="100%"> Ao longo dos anos</v-btn>
+      <v-btn v-if="!usuarioEstaLogado" class="mt-3" color="secondary" block max-width="100%"> Benefícios do Projeto</v-btn>
 
       <br>
-      <hr>
-      <v-btn to="/home" class="mt-3" color="secondary" block max-width="100%"> Página Inicial</v-btn>
-      <v-btn to="/configs" class="mt-3" color="secondary" block max-width="100%"> Configurações</v-btn>
-      <v-btn to="/admusers" class="mt-3" color="secondary" block max-width="100%"> Adm Usuários</v-btn>
-      <v-btn to="/tipoeventos" class="mt-3" color="secondary" block max-width="100%"> Adm Tipo Evento</v-btn>
+      <hr v-if="usuarioEstaLogado">
+      <v-btn v-if="usuarioEstaLogado" to="/home" class="mt-3" color="secondary" block max-width="100%"> Página Inicial
+      </v-btn>
+      <v-btn v-if="usuarioEstaLogado" to="/configs" class="mt-3" color="secondary" block max-width="100%"> Configurações
+      </v-btn>
+      <v-btn v-if="usuarioEstaLogado" to="/admusers" class="mt-3" color="secondary" block max-width="100%"> Adm Usuários
+      </v-btn>
+      <v-btn v-if="usuarioEstaLogado" to="/tipoeventos" class="mt-3" color="secondary" block max-width="100%"> Adm Tipo
+        Evento
+      </v-btn>
 
     </v-col>
   </v-row>
 </template>
 
 <script>import config from '../../http/config'
+import {logoutMixin} from '@/mixins'
+import {mapGetters} from 'vuex'
 
 export default {
+  mixins: [logoutMixin],
   data: () => ({
-    configSis: config,
-    messages: [
-      {
-        from: 'You',
-        message: `Sure, I'll see you later.`,
-        time: '10:42am',
-        color: 'deep-purple lighten-1'
-      },
-      {
-        from: 'John Doe',
-        message: 'Yeah, sure. Does 1:00pm work?',
-        time: '10:37am',
-        color: 'green'
-      },
-      {
-        from: 'You',
-        message: 'Did you still want to grab lunch today?',
-        time: '9:47am',
-        color: 'deep-purple lighten-1'
-      }
-    ]
+    configSis: config
   }),
-  computed: {},
+  computed: {
+    ...mapGetters(['usuarioResetado', 'usuarioLogado', 'usuarioEstaLogado'])
+  },
   mounted () {
   },
   methods: {}
